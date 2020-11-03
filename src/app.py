e22113453f9d83c08a48a91b5c6f3fcd46262b58
Sandro Matter <github@sandromatter.ch>
2020-11-03 16:11:11 +0100

    Set up project structure and flask with bootstrap 5

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Flask app initialization
-app = Flask(__name__)
+app = Flask(__name__, template_folder="frontend", static_url_path="/frontend/static", static_folder="frontend/static")
 
 # Flask app logic
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def index():
+    # Home page
+    return render_template("/pages/index.html", title="Pyfeed", description="The only feedburner you want in 2020.")
+
+if __name__ == "__main__":
+    app.run(debug=True)
